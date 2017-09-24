dd4ac0474223eb5be75358bebc2151f4f56a4de8
liufan2312 <liufan2312@gmail.com>
2017-09-24 14:45:34 -0400

    modified file

--- decisionTree/node.py
+++ decisionTree/node.py
@@ -12,17 +12,16 @@ class node(object):
         self.left_kid = None
         self.right_kid = None
 
-
     def split(self, col_index, split_value):
         n_s, n_f = self.data.shape
         data = zip(self.data, self.label)
-        data = sorted(self.data, key = self.data[0][:,col_index])
-        ind = data[0][:,col_index] <= split_value
-        ind  = min([item for item in ind if item])
+        data = sorted(self.data, key=self.data[0][:, col_index])
+        ind = data[0][:, col_index] <= split_value
+        ind = min([item for item in ind if item])
         if ind:
-            self.left_kid = node(data[0][:,0:ind], data[1][0:ind], self.type_list)
+            self.left_kid = node(data[0][:, 0:ind], data[1][0:ind], self.type_list)
         if ind+1<n_f:
-            self.right_kid = node(data[0][:,(ind+1):], data[1][(ind+1):], self.type_list)
+            self.right_kid = node(data[0][:, (ind+1):], data[1][(ind+1):], self.type_list)
 
 
 
